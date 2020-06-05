87ebd0e359543d8067c30ec71aa3c2da864b1fc1
Kent C. Dodds <me+github@kentcdodds.com>
2020-06-04 20:35:25 -0600

    exercises/05-routing

--- src/utils/api-client.js
+++ src/utils/api-client.js
@@ -1,8 +1,8 @@
-import {queryCache} from 'react-query'
-import * as auth from 'auth-provider'
+const localStorageKey = '__bookshelf_token__'
+
 const apiURL = process.env.REACT_APP_API_URL
 
-async function client(
+function client(
   endpoint,
   {data, token, headers: customHeaders, ...customConfig} = {},
 ) {
@@ -33,9 +33,8 @@ async function client(
   })
 }
 
-async function logout() {
-  queryCache.clear()
-  await auth.logout()
+function logout() {
+  window.localStorage.removeItem(localStorageKey)
 }
 
 export {client, logout, apiURL}
